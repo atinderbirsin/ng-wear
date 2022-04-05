66ec5e9621f4d93cf2e5b793c47e3a8883cc5c49
atinderbirsin <atinderbir3@gmail.com>
2022-04-05 18:00:14 +0530

    working on nav bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {
   Nav,
   NavbarContainer,
@@ -7,8 +7,71 @@ import {
   Icon,
 } from "./NavbarElements";
 import Logo from "../images/logooo.webp";
+import "./Nav.css";
 
 export function NavBar() {
+  // const ref = useRef(null);
+  // const NavbarRef = useRef(null);
+
+  // window.addEventListener("scroll", () => {
+  //   console.log(ref);
+  // });
+
+  // const func = (entries) => {
+  //   const [entry] = entries;
+
+  // if (!entry.isIntersecting) {
+  //   ref.current.classList.add("sticky");
+  //   NavbarRef.current.setAttribute("id", "sticks");
+  // } else {
+  //   ref.current.classList.remove("sticky");
+  //   NavbarRef.current.removeAttribute("id");
+  // }
+
+  // if (!entry.isIntersecting) {
+  //   console.log(entry);
+  //   ref.current.classList.add("sticky");
+  //   NavbarRef.current.setAttribute("id", "sticks");
+  // } else {
+  //   console.log(entry);
+  //   ref.current.classList.remove("sticky");
+  // }
+
+  // console.log(entry);
+  // };
+
+  // const options = {
+  //   root: null,
+  //   rootMargin: "0px",
+  //   threshold: 1,
+  // };
+
+  // useEffect(() => {
+  // const nav = ref.current;
+
+  // const navObserver = new IntersectionObserver(func, options);
+
+  // if (ref.current) navObserver.observe(ref.current);
+
+  // return () => {
+  //   if (nav) navObserver.unobserve(nav);
+  // };
+  // }, [ref, options]);
+
+  // navObserver.observe(ref.current);
+
+  // const top = ref.current.offsetTop;
+
+  // window.addEventListener("scroll", () => {
+  //   if (ref.current.offsetTop >= 0) {
+  //     ref.current.classList.add("sticky");
+  //     NavbarRef.current.setAttribute("id", "sticks");
+  //   } else {
+  //     ref.current.classList.remove("sticky");
+  //     NavbarRef.current.removeAttribute("id");
+  //   }
+  // });
+
   return (
     <>
       <Nav>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import BannerContainer from "../BannerContainer";
 import CardsContainer from "../CardsContainer";
 import ExclusiveSection from "../ExclusiveSection";
@@ -13,10 +13,42 @@ import Testimonials from "../testimonials";
 import Footer from "../Footer";
 
 const Home = () => {
+  const NavBarRef = useRef(null);
+  const BannerRef = useRef(null);
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) {
+      NavBarRef.current.classList.add("sticky");
+    } else {
+      NavBarRef.current.classList.add("remove");
+    }
+  };
+
+  const options = {
+    root: null,
+    rootMargin: "-88px",
+    threshold: 0,
+  };
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      callbackFunction,
+      options
+    );
+
+    if (BannerRef.current) intersectionObserver.observe(BannerRef.current);
+
+    return () => {
+      if (BannerRef.current) intersectionObserver.unobserve(BannerRef.current);
+    };
+  });
+
   return (
     <>
-      <NavBar />
-      <BannerContainer />
+      <NavBar ref={NavBarRef} />
+      <BannerContainer ref={BannerRef} />
       <ExclusiveSection />
       <HeroVideoSection />
       <ServicesContainer />
